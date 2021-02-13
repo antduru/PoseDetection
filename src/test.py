859455527b4191a19b6c56cbac4d2bf25b3bec21
Ufuk Bombar <ufukbombar@bordatech.com>
2021-02-14 02:00:17 +0300

    Refactor 2

--- src/test.py
+++ src/test.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 import collections
 import copy
 import torchvision
-from models import CroppedVgg19
+import models
 
 
 with torch.no_grad():
-    cropped_model = CroppedVgg19().eval()
+    cropped_model = models.CroppedVgg19().eval()
 
     image = torch.zeros((1, 3, 256, 256))
 
